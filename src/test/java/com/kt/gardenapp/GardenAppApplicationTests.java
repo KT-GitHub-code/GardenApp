7e167726f314152b5c2f01ce2e23adfbbe19c2a7
KT <71466471+KT-GitHub-code@users.noreply.github.com>
2022-10-06 15:47:00 +0200

    Add initial commit

---
+++ src/test/java/com/kt/gardenapp/GardenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.gardenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
