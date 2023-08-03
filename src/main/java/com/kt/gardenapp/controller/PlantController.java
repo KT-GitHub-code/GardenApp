0a4d17c6b683639823422e90a2a3493caa8b6878
KT <krakovszkyt@gmail.com>
2023-08-03 11:47:10 +0200

    Remove unused import

--- src/main/java/com/kt/gardenapp/controller/PlantController.java
+++ src/main/java/com/kt/gardenapp/controller/PlantController.java
@@ -1,7 +1,6 @@
 package com.kt.gardenapp.controller;
 
 import com.kt.gardenapp.model.Plant;
-import com.kt.gardenapp.repository.PlantRepository;
 import com.kt.gardenapp.service.PlantService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
