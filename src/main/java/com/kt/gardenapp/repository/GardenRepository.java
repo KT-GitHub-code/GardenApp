e3afb29d71726c1b815138a0e225c56221443f85
KT <krakovszkyt@gmail.com>
2023-08-09 16:05:47 +0200

    Add Repository annotation

--- src/main/java/com/kt/gardenapp/repository/GardenRepository.java
+++ src/main/java/com/kt/gardenapp/repository/GardenRepository.java
@@ -2,6 +2,8 @@ package com.kt.gardenapp.repository;
 
 import com.kt.gardenapp.model.Garden;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GardenRepository extends JpaRepository<Garden, Long> {
 }
--- src/main/java/com/kt/gardenapp/repository/PlantRepository.java
+++ src/main/java/com/kt/gardenapp/repository/PlantRepository.java
@@ -2,6 +2,8 @@ package com.kt.gardenapp.repository;
 
 import com.kt.gardenapp.model.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlantRepository extends JpaRepository<Plant, Long> {
 }
